164611e6958724acfa7261f5a05aaa43f881c374
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-08-03 13:25:04 +0200

    Update script.js
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ function insertHTML(_id, _content) {
     console.error(`The element '${_id}' could not be found`);
   }
 }
+
+function connectToDatabase(_url, _user, _password) {
+  fetch(`${_url}/${_user}/data.html?password=${_password}`)
+  .then((response) => response.json())
+  .then((posts) => {
+    container.innerHTML = "";
+    posts.forEach((post) => {
+      const div = cardTemplate.content.cloneNode(true);
+      div.getElementById("card-link").href = post.link;
+      div.getElementById("logo-img").src = post.logoImage;
+      div.getElementById("card-title").textContent = post.title;
+      div.getElementById("card-details").textContent = post.details;
+      div.getElementById("cover-img").src = post.coverImage;
+      div.getElementById(
+        "card-footer"
+      ).innerHTML = ` <ion-icon name="arrow-up"></ion-icon>
+          <ion-icon name="chatbox-ellipses"></ion-icon>
+          <ion-icon name="bookmark"></ion-icon>`;
+      container.append(div);
+    });
+  });
+ }
+
+
+
+
+
